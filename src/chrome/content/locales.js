693d66973efabd22439a31dd2952dc3412a092f3
Marko Zabreznik <marko@zabreznik.net>
2015-08-05 12:36:30 +0200

    german locale fix

--- src/chrome/content/locales.js
+++ src/chrome/content/locales.js
@@ -14,7 +14,13 @@ FxKeyboardLocales.en = {
     name: 'English',
     locale: 'en',
     main: [[
-        [{'label': "\u21E5", special: 'tab', flex: '1', class: 'fxKeyboardActionKeys tab', 'tooltiptext': 'Go to next input'}], // tab
+        [{
+            'label': "\u21E5",
+            special: 'tab',
+            flex: '1',
+            class: 'fxKeyboardActionKeys tab',
+            'tooltiptext': 'Go to next input'
+        }], // tab
         ['q', 'Q', '1', {label: 'Tab', string: '\t', flex: 1}],
         ['w', 'W', '2', ''],
         ['e', 'E', '3', ''],
@@ -26,7 +32,7 @@ FxKeyboardLocales.en = {
         ['o', 'O', '9', ''],
         ['p', 'P', '0', ''],
         [{'label': "\u232B", special: 8, flex: '1', type: 'repeat', class: 'fxKeyboardActionKeys backspace'}] // backspace
-    ],[
+    ], [
         [{'label': "\u2325", flex: '3', special: 'alt', class: 'fxKeyboardActionKeys alt'}], // alt
         ['a', 'A', '#', '€'],
         ['s', 'S', '$', '£'],
@@ -51,10 +57,22 @@ FxKeyboardLocales.en = {
         ['.', ':', '×', ''],
         ['?', '!', '÷', '¡'],
         ['@', '_', '=', '¿']
-    ],[
-        [{'label': "\uD83C\uDF10", special: 'toggleLocale', flex: '1', class: 'fxKeyboardActionKeys toggleLocale narrow', 'tooltiptext': 'Toggle locale'}], // keep open
+    ], [
+        [{
+            'label': "\uD83C\uDF10",
+            special: 'toggleLocale',
+            flex: '1',
+            class: 'fxKeyboardActionKeys toggleLocale narrow',
+            'tooltiptext': 'Toggle locale'
+        }], // keep open
         [{'label': "", flex: '2', char: 32}], // space
-        [{'label': "\uD83D\uDD12", special: 'keepOpen', flex: '1', class: 'fxKeyboardActionKeys keepOpen narrow', 'tooltiptext': 'Keep open'}] // keep open
+        [{
+            'label': "\uD83D\uDD12",
+            special: 'keepOpen',
+            flex: '1',
+            class: 'fxKeyboardActionKeys keepOpen narrow',
+            'tooltiptext': 'Keep open'
+        }] // keep open
     ]]
 };
 
@@ -62,7 +80,13 @@ FxKeyboardLocales.de = {
     name: 'Deutsch',
     locale: 'de',
     main: [[
-        [{'label': "\u21E5", special: 'tab', flex: '1', class: 'fxKeyboardActionKeys tab', 'tooltiptext': 'Go to next input'}], // tab
+        [{
+            'label': "\u21E5",
+            special: 'tab',
+            flex: '1',
+            class: 'fxKeyboardActionKeys tab',
+            'tooltiptext': 'Go to next input'
+        }], // tab
         ['q', 'Q', '1', {label: 'Tab', string: '\t', flex: 1}],
         ['w', 'W', '2', ''],
         ['e', 'E', '3', ''],
@@ -90,9 +114,20 @@ FxKeyboardLocales.de = {
         ['ö', 'Ö', '', ''],
         ['ä', 'Ä', '', ''],
         [{'label': "\u23ce", flex: '3', special: 13, class: 'fxKeyboardActionKeys enter'}] // enter
-    ],
-        FxKeyboardLocales.en.main[2],
-        FxKeyboardLocales.en.main[3]
+    ], [
+        [{'label': "\u21E7", flex: '5', special: 'shift', class: 'fxKeyboardActionKeys shift'}], // shift / caps
+        ['y', 'Y', '\'', ''],
+        ['x', 'X', '"', ''],
+        ['c', 'C', '/', '['],
+        ['v', 'V', '\\', ']'],
+        ['b', 'B', '<', '{'],
+        ['n', 'N', '>', '}'],
+        ['m', 'M', '+', ''],
+        [',', ';', '-', ''],
+        ['.', ':', '×', ''],
+        ['?', '!', '÷', '¡'],
+        ['@', '_', '=', '¿']
+    ], FxKeyboardLocales.en.main[3]
     ]
 };
 
@@ -100,7 +135,13 @@ FxKeyboardLocales.da = {
     name: 'Dansk',
     locale: 'da',
     main: [[
-        [{'label': "\u21E5", special: 'tab', flex: '1', class: 'fxKeyboardActionKeys tab', 'tooltiptext': 'Go to next input'}], // tab
+        [{
+            'label': "\u21E5",
+            special: 'tab',
+            flex: '1',
+            class: 'fxKeyboardActionKeys tab',
+            'tooltiptext': 'Go to next input'
+        }], // tab
         ['q', 'Q', '1', {label: 'Tab', string: '\t', flex: 1}],
         ['w', 'W', '2', ''],
         ['e', 'E', '3', ''],
@@ -137,7 +178,13 @@ FxKeyboardLocales.sl = {
     name: 'South Slavic Latin',
     locale: 'sl',
     main: [[
-        [{'label': "\u21E5", special: 'tab', flex: '1', class: 'fxKeyboardActionKeys tab', 'tooltiptext': 'Go to next input'}], // tab
+        [{
+            'label': "\u21E5",
+            special: 'tab',
+            flex: '1',
+            class: 'fxKeyboardActionKeys tab',
+            'tooltiptext': 'Go to next input'
+        }], // tab
         ['q', 'Q', '1', {label: 'Tab', string: '\t', flex: 1}],
         ['w', 'W', '2', ''],
         ['e', 'E', '3', ''],
